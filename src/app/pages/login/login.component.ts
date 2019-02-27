0ad1d59393271567ad41bec0281004d649af0ffd
hc3 <eliel.floyd@bol.com.br>
2019-02-27 18:31:41 -0300

    atualizando login e configuração do firebase.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '@core/data/login.service';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ import { LoginService } from '@core/data/login.service';
 export class LoginComponent implements OnInit {
 
   loginForm:FormGroup;
-  user:any={};
+  user:User;
   submited:boolean=false;
   loading:boolean=false;
   customLoadingTemplate:any;
@@ -20,29 +21,41 @@ export class LoginComponent implements OnInit {
     public formBuilder:FormBuilder,
     public route:Router,
     public loginService: LoginService
-  ) { }
+  ) 
+  { 
+    this.loginForm = this.formBuilder.group({
+      email:['',],
+      password:['',]
+    })
+  }
 
   ngOnInit() {
   }
 
-  public login() {
+  public onSubmit() {
+
     this.submited = true;
     this.loading = true;
 
     if(this.loginForm.invalid) {
       this.loading = false;
       return;
-    };
+    } else {
+      
+      this.user = new User(this.loginForm);
 
-    this.loginService.login(this.user)
-      .then((result) => {
-        this.loading = false;
-        this.route.navigate(['/home']);
-      })
-      .catch((err) => {
-        this.loading = false;
-        console.log('err');
-      })
+      this.loginService.login(this.user)
+        .then(() => {
+          this.loading = false;
+          this.route.navigate(['/home']);
+        })
+        .catch((err) => {
+          this.loading = false;
+          console.log('err',err);
+        })
+    }
+
+
 
   }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,14 +7,13 @@ export const environment = {
 };
 
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBs_hCaz2ASchYF7mhuVEybt6gDXPH-Gmc",
-  authDomain: "dogindb.firebaseapp.com",
-  databaseURL: "https://dogindb.firebaseio.com",
-  projectId: "dogindb",
-  storageBucket: "dogindb.appspot.com",
-  messagingSenderId: "486075167829"
+  apiKey: "AIzaSyBy0ep-bySgYpy_YxMQcOq6wC1SSW2ELhA",
+  authDomain: "dogin-201904.firebaseapp.com",
+  databaseURL: "https://dogin-201904.firebaseio.com",
+  projectId: "dogin-201904",
+  storageBucket: "dogin-201904.appspot.com",
+  messagingSenderId: "582465888736"
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
